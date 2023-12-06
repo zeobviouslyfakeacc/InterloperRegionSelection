2377d48c851c4eeeca2de4a4e564eb27b28f3c7a
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-12-06 01:21:31 -0500

    Update for MelonLoader 6 and TLD 2.25 (#2)
    * Update for MelonLoader 6 and TLD 2.04
    
    * Use nuget package
    
    * Change nuget package and bump version

--- Patches.cs
+++ Patches.cs
@@ -1,78 +1,17 @@
-﻿using System;
-using System.Linq;
-using HarmonyLib;
+﻿using HarmonyLib;
+using Il2Cpp;
 
-namespace InterloperRegionSelection {
+namespace InterloperRegionSelection
+{
 	internal class Patches {
-
-		private static bool overrideSceneToLoad = false;
-
-		[HarmonyPatch(typeof(GameManager), "RegionLockedBySelectedMode", new Type[0])]
-		private static class UnlockInterloperRegionSelection {
-			private static bool Prefix(ref bool __result) {
-				if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Interloper) {
-					__result = false;
-					return false;
-				} else {
-					return true;
-				}
-			}
-		}
-
-		[HarmonyPatch(typeof(Panel_SelectRegion_Map), "OnSelectRegionContinue", new Type[0])]
-		private static class OverrideSceneLoadWhenComingFromInterloperRegionSelection {
-			private static void Prefix() {
-				overrideSceneToLoad = (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Interloper);
-			}
-		}
-
-		[HarmonyPatch(typeof(Panel_SelectRegion_Map), "OnClickBack", new Type[0])]
-		private static class DontOverrideSceneLoadWhenCancellingRegionSelect {
-			private static void Prefix(Panel_SelectRegion_Map __instance) {
-				if (!__instance.m_PreviousSelectedItem) {
-					overrideSceneToLoad = false;
-				}
-			}
-		}
-
-		[HarmonyPatch(typeof(GameManager), "LoadSceneWithLoadingScreen")]
-		private static class SetSceneToBeLoaded {
-			private static void Prefix(ref string sceneName) {
-				if (!overrideSceneToLoad) return;
-
-				GameRegion startRegion = GameManager.m_StartRegion;
-				if (startRegion != GameRegion.RandomRegion && startRegion != GameRegion.FutureRegion) {
-					sceneName = GameManager.m_StartRegion.ToString();
-				}
-				overrideSceneToLoad = false;
-			}
-		}
-
-		[HarmonyPatch(typeof(Panel_SelectRegion_Map), "Enable", new Type[] { typeof(bool) })]
-		private static class EnsureOnlyInterloperRegionsSelectable {
-			private static void Postfix(Panel_SelectRegion_Map __instance, bool enable) {
-				if (!enable) {
-					return;
-				}
-
-				if (ExperienceModeManager.GetCurrentExperienceModeType() == ExperienceModeType.Interloper) {
-					GameRegion[] interloperRegions = GameManager.Instance().m_SandboxConfig.m_InterloperRegions;
-					foreach (SelectRegionItem item in __instance.m_Items) {
-						item.gameObject.SetActive(interloperRegions.Contains(item.m_Region));
-					}
-				} else {
-					foreach (SelectRegionItem item in __instance.m_Items) {
-						item.gameObject.SetActive(true);
+		[HarmonyPatch(typeof(Panel_SelectExperience), nameof(Panel_SelectExperience.Enable), new Type[] { typeof(bool) })]
+		private static class InterlopersCanChooseWhereTheyInterlope {
+			private static void Postfix(Panel_SelectExperience __instance) {
+				foreach (var item in __instance.m_MenuItems) {
+					if (item.m_SandboxConfig.m_XPMode.m_ModeType == ExperienceModeType.Interloper) {
+						item.m_SandboxConfig.m_StartRegionSelectionBlocked = false;
 					}
 				}
-			}
-		}
-
-		[HarmonyPatch(typeof(Panel_SelectRegion_Map), "SelectItem")]
-		private static class DisableControllerSelectionOfInvalidInterloperRegions {
-			private static bool Prefix(SelectRegionItem item) {
-				return ExperienceModeManager.GetCurrentExperienceModeType() != ExperienceModeType.Interloper
-					|| !item || GameManager.Instance().m_SandboxConfig.m_InterloperRegions.Contains(item.m_Region);
 			}
 		}
 	}
